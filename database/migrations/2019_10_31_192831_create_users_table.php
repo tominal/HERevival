34ebde476360822b6b3096001ebcdf06a9c6aaf3
Mikkel Friis <mikkelbfriis@hotmail.com>
2019-10-31 20:47:49 +0100

    Initial commit

---
+++ database/migrations/2019_10_31_192831_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('username');
+            $table->string('password');
+            $table->string('game_ip');
+            $table->string('ip_address');
+            $table->boolean('is_admin');
+            $table->boolean('is_learning');
+            $table->dateTime('premium');
+            $table->dateTime('last_login');
+
+            $table->unique('id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('users');
+    }
+}
