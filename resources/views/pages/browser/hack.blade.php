6e4a14b956d605e1c6e33fb587d21bbdf5a0dcf1
Mikkel Friis <mikkelbfriis@hotmail.com>
2019-11-04 00:23:39 +0100

    template for hack and login on servers

--- resources/views/pages/browser/hack.blade.php
+++ resources/views/pages/browser/hack.blade.php
@@ -1,7 +1,14 @@
 @extends('pages.browser.layouts.browser')
 
 @section('browser_content')
-
-    Hack page - two fckin buttons - goes here
-
-@stop
+    <div class="row">
+        <div class="col-md-6 offset-md-4">
+            <button class="btn btn-lg btn-primary">
+                <i class="fad fa-lock"></i><br>Bruteforce attack
+            </button>
+            <button class="btn btn-lg btn-primary">
+                <i class="fad fa-bug"></i><br>Exploit attack
+            </button>
+        </div>
+    </div>
+@endsection
--- resources/views/pages/browser/login.blade.php
+++ resources/views/pages/browser/login.blade.php
@@ -1,5 +1,29 @@
 @extends('pages.browser.layouts.browser')
 
 @section('browser_content')
-    Login page here
-@stop
+    <div class="card col-md-4 offset-md-4">
+        <div class="card-body">
+            <form method="POST">
+                @csrf
+                <div class="form-group row">
+                    <div class="col-md-12">
+                        <input id="email" type="email" class="form-control" placeholder="{{ __('Username') }}">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <div class="col-md-12">
+                        <input id="password" type="password" class="form-control" name="password" placeholder="{{ __('Password') }}">
+                    </div>
+                </div>
+                <div class="form-group row mb-0">
+                    <div class="col-md-12">
+                        <button type="submit" class="btn btn-primary btn-block">
+                            {{ __('Login') }}
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
